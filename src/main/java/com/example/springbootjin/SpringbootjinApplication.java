b27258e1c2649399fb374dc48bf146f4bafcfe2b
jin936424 <913528904@qq.com>
2021-10-25 21:10:19 +0800

    java调用c端接口
    java调用c端接口

---
+++ src/main/java/com/example/springbootjin/SpringbootjinApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootjin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootjinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootjinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootjin/SpringbootjinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootjin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootjinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
